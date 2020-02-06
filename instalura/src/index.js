d10cbcfda97cc80e7c3f8bf678c82937b5ba1c2a
Isabella Soares <isabella.lima@goomer.com.br>
2020-02-06 15:37:43 -0300

    Tentando começar a aula 3

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './css/timeline.css';
 import './css/reset.css';
-// import * as serviceWorker from './serviceWorker';
+import './css/login.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
