1a0e67b3a313bef010fb1d06276dc9800dfe6ba3
Isabella <isabella.soares.28@gmail.com>
2020-02-22 16:52:01 -0300

    Add redux and redux-thunk

--- instalura/src/App.js
+++ instalura/src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import './css/timeline.css';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
-import { createStore } from 'redux';
-import { timeline } from './reducers/timeline';
+import {createStore,applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {timeline} from './reducers/timeline';
 
-const store = createStore(timeline);
+const store = createStore(timeline,applyMiddleware(thunkMiddleware));
 
 class App extends Component {
-	render() {
-		return (
-			<div id="root">
-				<div data-reactroot="" className="main">
-					<Header />
-					<Timeline login={this.props.params.login} store={store} />
-				</div>
-			</div>
-		);
-	}
+  render() {    
+    return (
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline login={this.props.params.login} store={store}/>
+      </div>
+    </div>
+    );
+  }
 }
 
 export default App;
--- instalura/src/components/Timeline.js
+++ instalura/src/components/Timeline.js
@@ -14,7 +14,7 @@ export default class Timeline extends Component {
     componentWillMount(){
       this.props.store.subscribe(() => {
         this.setState({fotos:this.props.store.getState()});
-      });
+      })
     }
 
     carregaFotos(){  
@@ -24,13 +24,9 @@ export default class Timeline extends Component {
         urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
       } else {
         urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
-      }
+      } 
 
-      const listaFixa = [{"urlPerfil":"https://s3.amazonaws.com/loa-production-23ffs35gui41a/writers/images/000/000/187/big/lincoln_abraham_WD.jpg?1458837750","loginUsuario":"alots","horario":"09/02/2020 23:21","urlFoto":"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2018/02/thomas-edison-1.jpg","id":1,"likeada":false,"likers":[],"comentarios":[],"comentario":"Wow que legal!"},{"urlPerfil":"https://s3.amazonaws.com/loa-production-23ffs35gui41a/writers/images/000/000/187/big/lincoln_abraham_WD.jpg?1458837750","loginUsuario":"alots","horario":"09/02/2020 23:21","urlFoto":"https://www.investors.com/wp-content/uploads/2016/03/LSpic_Franklin_031816_pd.jpg","id":2,"likeada":false,"likers":[],"comentarios":[],"comentario":"Isso é bom demais!"}];
-      
-      TimelineApi.lista(urlPerfil,this.props.store);
-      this.props.store.dispatch({type:'LISTAGEM',fotos:listaFixa});
-      // this.props.store.lista(urlPerfil);
+      this.props.store.dispatch(TimelineApi.lista(urlPerfil));                  
     }
 
     componentDidMount(){
@@ -45,11 +41,11 @@ export default class Timeline extends Component {
     }
 
     like(fotoId) {
-      this.props.store.like(fotoId);    
+      this.props.store.dispatch(TimelineApi.like(fotoId));
     }
 
-    comenta(fotoId, textoComentario) {
-      this.props.store.comenta(fotoId, textoComentario);   
+    comenta(fotoId,textoComentario) {
+      this.props.store.dispatch(TimelineApi.comenta(fotoId,textoComentario));
     }
 
     render(){
