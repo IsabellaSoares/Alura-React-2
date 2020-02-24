068bd94b0925576eafeb8b955760838f9f3769d8
Isabella <isabella.soares.28@gmail.com>
2020-02-24 16:02:27 -0300

    update react-redux

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css';
 import App from './App';
 import Login from './components/Login';
 import Logout from './components/Logout';
-import './css/timeline.css';
-import './css/reset.css';
-import './css/login.css';
 import {Router,Route,browserHistory} from 'react-router';
 import {matchPattern} from 'react-router/lib/PatternUtils';
+import {createStore,applyMiddleware,combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {timeline} from './reducers/timeline';
+import {notificacao} from './reducers/header';
+import {Provider} from 'react-redux';
 
 function verificaAutenticacao(nextState,replace) {
-    const resultado = matchPattern('/timeline(/:login)',nextState.location.pathname);
-    const enderecoPrivadoTimeline = resultado.paramValues[0] === undefined;
-    
-    if(enderecoPrivadoTimeline && localStorage.getItem('auth-token') === null){
-        replace('/?msg=você precisa estar logado para acessar o endereço');
-    }
+  const resultado = matchPattern('/timeline(/:login)',nextState.location.pathname);
+  const enderecoPrivadoTimeline = resultado.paramValues[0] === undefined;
+
+  if(enderecoPrivadoTimeline && localStorage.getItem('auth-token') === null){
+    replace('/?msg=você precisa estar logado para acessar o endereço');
+  }
 }
 
+const reducers = combineReducers({timeline,notificacao});
+const store = createStore(reducers,applyMiddleware(thunkMiddleware));
+
 ReactDOM.render(
-    (
-        <Router history={browserHistory}>
-            <Route path="/" component={Login}/>
-            <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao}/>      
-            <Route path="/logout" component={Logout}/>
-        </Router>
-    ),
-    document.getElementById('root')
+  (
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={Login}/>
+        <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao}/>      
+        <Route path="/logout" component={Logout}/>
+      </Router>
+    </Provider>
+  ),
+  document.getElementById('root')
 );
