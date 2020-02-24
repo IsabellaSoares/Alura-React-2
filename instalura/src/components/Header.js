a5b1dc02415f771a3333c9208df81b680a33ddf6
Isabella <isabella.soares.28@gmail.com>
2020-02-24 15:00:07 -0300

    add Immutable.js and combineReducers

--- instalura/src/components/Header.js
+++ instalura/src/components/Header.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
-import Pubsub from 'pubsub-js';
+import TimelineApi from '../logicas/TimelineApi';
 
 export default class Header extends Component {
-	pesquisa(event) {
-		event.preventDefault();
-		fetch(`http://localhost:8080/api/public/fotos/${this.loginPesquisado.value}`)
-			.then((response) => response.json())
-			.then((fotos) => {
-				Pubsub.publish('timeline', fotos);
-			});
-	}
 
-	render() {
-		return (
-			<header className="header container">
-				<h1 className="header-logo">Instalura</h1>
+    constructor(){
+      super();
+      this.state = {msg:''};
+    }
 
-				<form className="header-busca" onSubmit={this.pesquisa.bind(this)}>
-					<input
-						type="text"
-						name="search"
-						placeholder="Pesquisa"
-						className="header-busca-campo"
-						ref={(input) => (this.loginPesquisado = input)}
-					/>
-					<input type="submit" value="Buscar" className="header-busca-submit" />
-				</form>
+    componentDidMount(){
+      this.props.store.subscribe(() => {
+        this.setState({msg:this.props.store.getState().notificacao});
+      });
+    }
 
-				<nav>
-					<ul className="header-nav">
-						<li className="header-nav-item">
-							<a href="#">
-								♡
-								{/*                 ♥ */}
-								{/* Quem deu like nas minhas fotos */}
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		);
-	}
+    pesquisa(event){
+      event.preventDefault();
+      this.props.store.dispatch(TimelineApi.pesquisa(this.loginPesquisado.value));
+    }
+
+    render(){
+        return (
+        <header className="header container">
+          <h1 className="header-logo">
+            Instalura
+          </h1>
+
+          <form className="header-busca" onSubmit={this.pesquisa.bind(this)}>
+            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" ref={input => this.loginPesquisado = input}/>
+            <input type="submit" value="Buscar" className="header-busca-submit"/>
+          </form>
+
+
+          <nav>
+            <ul className="header-nav">
+              <li className="header-nav-item">
+                <span>{this.state.msg}</span>
+                <a href="#">
+                  ♡
+                  {/*                 ♥ */}
+                  {/* Quem deu like nas minhas fotos */}
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>            
+        );
+    }
 }
